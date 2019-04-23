ecb586a9f0205a1497a3f69f844f7c70df90feb8
Hariharan <cvhariharan@protonmail.com>
2019-04-23 22:51:07 +0530

    Sorting

--- customtype/tc_utils.go
+++ customtype/tc_utils.go
@@ -1,5 +1,14 @@
 package customtype
 
-func (t *TravelCapsule) AddPost() {
-	
+// Functions that help in sorting travelcapsules by date
+type TravelCapsules []TravelCapsule
+
+func (tc TravelCapsules) Len() int {
+    return len(tc)
+}
+func (tc TravelCapsules) Swap(i, j int) {
+    tc[i], tc[j] = tc[j], tc[i]
+}
+func (tc TravelCapsules) Less(i, j int) bool {
+    return tc[i].UpdatedOn.After(tc[j].UpdatedOn)
 }
